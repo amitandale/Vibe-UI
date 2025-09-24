40494bcc8568d6ece248a706e6b3790d56c92106
Alejandro <alejandro.uliski@gmail.com>
2025-09-24 00:02:22 -0500

    ci(vibe-saas): run Supabase Local SQL tests on self-hosted Windows runner

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
-/**
- * Next.js config (CJS) for Next 15
- * - Removes deprecated experimental.appDir
- * - Adds aliases for legacy Next internals used by some deps
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
     config.resolve = config.resolve || {};
@@ -16,4 +11,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;

b71209f0e7d9e9dee42c81f07def6f9ef2296404
Alejandro <alejandro.uliski@gmail.com>
2025-09-23 23:55:20 -0500

    ci(vibe-saas): run Supabase Local SQL tests on self-hosted Windows runner

--- next.config.js
+++ next.config.js
@@ -7,4 +7,12 @@ export default {
     };
     return config;
   },
+  webpack: (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = { ...(config.resolve.alias || {}),
+      'next/dist/pages/_app': 'next/app',
+      'next/dist/pages/_document': 'next/document',
+    };
+    return config;
+  },
  experimental: { appDir: true } }
